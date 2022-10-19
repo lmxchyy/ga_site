7d8660f68dc371511f775f53990bec038ec59758
lmxchyy <lmxchyy@gmail.com>
2022-10-19 23:09:55 +0800

    hello

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Link from "gatsby-link"
 export default function Home() {
   return <div style ={{color: 'green'}}> 
-            <h1>Hello world</h1>
+            <h1>Hello world,这是我的第一Gatsby网站项目</h1>
             <p>this is a prograph</p>
            <Link to="/about">about</Link>
            <br />
