d6fd26cf4f42df375c59bad99f8b3aef70c7d6b4
lmxchyy <lmxchyy@gmail.com>
2022-10-20 15:28:58 +0800

    pluginsadd

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,3 +8,44 @@ module.exports = {
   /* Your site config here */
   plugins: [],
 }
+
+module.exports = {
+  siteMetadata: {
+    title: "My Cool Website-akdiving",
+    author: "lmxchyy"
+  },
+  plugins: ["gatsby-plugin-sitemap"],
+}
+
+module.exports = {
+  siteMetadata: {
+    title: "My Cool Website-diving",
+    author: "lmxchyy"
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        output: `/sitemap`,
+      },
+    },
+  ],
+}
+
+
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const nodeData = {
+    title: "Test Node",
+    description: "Testing the node ",
+  }
+  const newNode = {
+    ...nodeData,
+    id: createNodeId("TestNode-testid"),
+    internal: {
+      type: "TestNode",
+      contentDigest: createContentDigest(nodeData),
+    },
+  }
+  actions.createNode(newNode)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Link from "gatsby-link"
 import Counter from "./counter"
-export default function Home() {
+import { graphql } from "gatsby"
+export default function Home({data}) {
   return <div style ={{color: 'green'}}> 
             <h1>Hello world,这是我的第一Gatsby网站项目</h1>
             <p>this is a prograph</p>
+            <p>{data.site.siteMetadata.title}</p>
+            <h3>{data.site.siteMetadata.author}</h3>
            <Link to="/about">about</Link>
            <br />
            <Link to="/company/history/"> history</Link>
@@ -12,3 +15,16 @@ export default function Home() {
            <Counter color="red"/>           
         </div>
 }
+
+export const query = graphql`
+  query FisrtQuery {
+    site {
+      siteMetadata{
+        title
+        author
+      }
+    }
+  }
+
+
+`
